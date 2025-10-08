4ed551b47835a811f44c2518b8afd2cf4dd5ff11
Shikhar Dave <b22ch032@iitj.ac.in>
2025-10-08 08:37:45 +0530

    type

--- src/models/equipment.py
+++ src/models/equipment.py
@@ -1,3 +1,20 @@
 from typing import Optional, Dict, List
 from pydantic import BaseModel, Field
 from enum import Enum
+
+class EquipmentType(str, Enum):
+    """Standard equipment types in process flowsheets"""
+    REACTOR = "reactor"
+    HEAT_EXCHANGER = "heat_exchanger"
+    DISTILLATION = "distillation_column"
+    MIXER = "mixer"
+    SPLITTER = "splitter"
+    PUMP = "pump"
+    COMPRESSOR = "compressor"
+    VALVE = "valve"
+    TANK = "tank"
+    SEPARATOR = "separator"
+    RAW_MATERIAL = "raw_material"
+    PRODUCT = "product"
+    UTILITY = "utility"
+    UNKNOWN = "unknown"
